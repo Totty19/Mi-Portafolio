2d6f929f4b858ffa09eb1f49429c7b2023ce917f
Totty19 <tomiexe16@gmail.com>
2023-07-15 17:07:47 -0300

    actualizando nav

--- src/Common/NavBar/index.jsx
+++ src/Common/NavBar/index.jsx
@@ -1,29 +1,14 @@
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-
 const BarNav = () => {
     return (
         <>
-            {['md',].map((expand) => (
-                <Navbar key={expand} expand={expand}>
-                    <Container fluid className="contExpand">
-                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                        <Navbar.Offcanvas id={`offcanvasNavbar-expand-${expand}`} aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} className='navBarExpand' placement="end">
-                            <Offcanvas.Body>
-                                <Nav className="justify-content-center flex-grow-1 pe-3 navBar">
-                                    <Nav.Link href="#sobre-mi" className='navLink'>sobre mi</Nav.Link>
-                                    <Nav.Link href="#habilidades" className='navLink'>habilidades</Nav.Link>
-                                    <Nav.Link href="#proyectos" className='navLink'>proyectos</Nav.Link>
-                                    <Nav.Link href="#certificados" className='navLink'>certificados</Nav.Link>
-                                </Nav>
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
-                    </Container>
-                </Navbar>
-        ))}
-    </>     
+            <Nav className="justify-content-center text-center flex-grow-1 pe-3 navBar">
+                <Nav.Link href="#sobre-mi" className='navLink'>sobre mi</Nav.Link>
+                <Nav.Link href="#habilidades" className='navLink'>habilidades</Nav.Link>
+                <Nav.Link href="#proyectos" className='navLink'>proyectos</Nav.Link>
+                <Nav.Link href="#certificados" className='navLink'>certificados</Nav.Link>
+            </Nav>
+        </>     
     );
 }
 
