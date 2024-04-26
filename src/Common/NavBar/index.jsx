7cbd51ecc310d038d7b4e72eab984169c5e11a1b
Totty19 <tomiexe16@gmail.com>
2024-04-26 15:49:33 -0300

    Solucionando

--- src/Common/NavBar/index.jsx
+++ src/Common/NavBar/index.jsx
@@ -1,14 +1,17 @@
 import Nav from 'react-bootstrap/Nav';
+import img from '../imgs/Vector.png'
+
 const BarNav = () => {
     return (
-        <>
-            <Nav className="justify-content-center text-center flex-grow-1 pe-3 navBar">
+        <div className='ContNav'>
+            <img src={img} alt="Bars" className='bars'/>
+            <Nav className="navBar">
                 <Nav.Link href="#sobre-mi" className='navLink'>sobre mi</Nav.Link>
                 <Nav.Link href="#habilidades" className='navLink'>habilidades</Nav.Link>
                 <Nav.Link href="#proyectos" className='navLink'>proyectos</Nav.Link>
                 <Nav.Link href="#certificados" className='navLink'>certificados</Nav.Link>
             </Nav>
-        </>     
+        </div>     
     );
 }
 
